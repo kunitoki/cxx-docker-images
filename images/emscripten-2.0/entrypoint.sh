e39dcfaabe25fee843cbb82b79b400fe93c26b02
kunitoki <kunitoki@gmail.com>
2022-09-20 00:13:22 +0200

    Update entrypoint.sh

--- images/emscripten-2.0/entrypoint.sh
+++ images/emscripten-2.0/entrypoint.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+# determine system node.js
+NODE=$(which node)
+sed -i'.old' "/^NODE_JS/s/= .*/= '${NODE//\//\\/}'/" /emsdk/.emscripten
+
 # set EMSDK environment variables
 . /emsdk/emsdk_env.sh > /dev/null
 
 # determine EMSCRIPTEN root path
 EMSCRIPTEN_COMPILER_PATH=`find /emsdk -name "emcc"`
 export EMSCRIPTEN=`dirname $EMSCRIPTEN_COMPILER_PATH`
-
-# export PATH
-export PATH="/usr/bin/cmake/bin:${PATH}"
 
 # execute called command
 exec "$@"
